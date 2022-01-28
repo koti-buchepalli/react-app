72bba5a38e64489f764dc15f5e5040be57d89daf
Koti Buchepalli <koti.buchepalli@hedgehoglab.com>
2022-01-28 22:28:12 +0000

    recomplied with core changed

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,13 +3,19 @@ const github = require('@actions/github');
 
 try {
     // throw new Error("mock error message")
+    core.debug("Sample debug");
+    core.warning("Sample warning");
+    core.error("Sample error")
     const name = core.getInput('who-to-greet');
     console.log(`Hello ${name}`);
-
+    core.setSecret(name);
+    console.log(`Hello ${name}`);
     const time = new Date();
     core.setOutput("time", time.toTimeString());
-
+    core.startGroup('Start group');
     console.log(JSON.stringify(github,null,'\t'));
+    core.endGroup();
+    core.exportVariable("HELLO", "hello");
 } catch (error) {
     core.setFailed(error.message)
 }
